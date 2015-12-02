4a4873b239a4bf207d1cc6720a6594cbcd243e2d
Jokuskay <tafanasyev@gmail.com>
2015-12-02 22:40:15 +0700

    Changed reply style

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -13,7 +14,7 @@ import (
 func main() {
 	bot, err := tgbotapi.NewBotAPI(telegramToken)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
 	bot.Debug = false
@@ -23,49 +24,58 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 10
 
-	updates, err := bot.GetUpdatesChan(u)
+	updates, _ := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		log.Println("got message")
-
 		if update.Message.Photo != nil {
+			log.Println("Got photo")
+
 			fileID := getMaxFileID(update.Message.Photo)
 			photoURL, err := bot.GetFileDirectURL(fileID)
 
 			if err != nil {
 				log.Panic(err)
-			} else {
-				jsonReq := []byte("{\"url\":\"" + photoURL + "\"}")
-
-				req, _ := http.NewRequest("POST", "https://api.projectoxford.ai/emotion/v1.0/recognize", bytes.NewBuffer(jsonReq))
-				req.Header.Add("Ocp-Apim-Subscription-Key", emotionsAPIKey)
-				req.Header.Add("Content-Type", "application/json")
-
-				client := &http.Client{}
-				resp, err := client.Do(req)
-				defer resp.Body.Close()
-
-				if err != nil {
-					log.Panic(err)
-				}
-
-				body, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					log.Panic(err)
-				} else {
-					var faces []Face
-					err := json.Unmarshal(body, &faces)
-					if err != nil {
-						log.Panic(err)
-					} else {
-						for _, face := range faces {
-							msg := tgbotapi.NewMessage(update.Message.Chat.ID, face.String())
-							msg.ReplyToMessageID = update.Message.MessageID
-							bot.Send(msg)
-						}
-					}
-				}
+				continue
 			}
+
+			jsonReq := []byte("{\"url\":\"" + photoURL + "\"}")
+
+			req, _ := http.NewRequest("POST", "https://api.projectoxford.ai/emotion/v1.0/recognize", bytes.NewBuffer(jsonReq))
+			req.Header.Add("Ocp-Apim-Subscription-Key", emotionsAPIKey)
+			req.Header.Add("Content-Type", "application/json")
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			defer resp.Body.Close()
+
+			if err != nil {
+				log.Panic(err)
+				continue
+			}
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Panic(err)
+				continue
+			}
+
+			var faces []Face
+			err = json.Unmarshal(body, &faces)
+			if err != nil {
+				log.Panic(err)
+				continue
+			}
+
+			if len(faces) == 0 {
+				log.Println("No faces")
+				continue
+			}
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, getFacesAsString(faces))
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
+
+			log.Println("Message sent")
 		}
 	}
 }
@@ -75,11 +85,33 @@ func getMaxFileID(photos []tgbotapi.PhotoSize) string {
 	width := photos[0].Width
 
 	for _, photo := range photos {
-		if photo.Width > width {
+		if width < photo.Width && photo.Width <= 4096 {
 			result = photo.FileID
 			width = photo.Width
 		}
 	}
 
 	return result
+}
+
+func getFacesAsString(faces []Face) string {
+	var buffer bytes.Buffer
+
+	isNeedNumeration := len(faces) > 1
+
+	for i, face := range faces {
+		if isNeedNumeration {
+			buffer.WriteString(fmt.Sprintf("\n#%d:\n", i+1))
+		}
+		buffer.WriteString(face.String())
+	}
+
+	return buffer.String()
+}
+
+func round(val float32) int {
+	if val < 0 {
+		return int(val - 0.5)
+	}
+	return int(val + 0.5)
 }
--- types.go
+++ types.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 // Face - emotion
 type Face struct {
@@ -28,6 +31,33 @@ type Scores struct {
 	Surprise  float32
 }
 
-func (f *Face) String() string {
-	return fmt.Sprintf("Anger: %.2f\nContempt: %.2f\nDisgust: %.2f\nFear: %.2f\nHappiness: %.2f\nNeutral: %.2f\nSadness: %.2f\nSurprise: %.2f", f.Scores.Anger, f.Scores.Contempt, f.Scores.Disgust, f.Scores.Fear, f.Scores.Happiness, f.Scores.Neutral, f.Scores.Sadness, f.Scores.Surprise)
+func (face *Face) String() string {
+	var buffer bytes.Buffer
+
+	if anger := round(face.Scores.Anger * 100); anger > 0 {
+		buffer.WriteString(fmt.Sprintf("Anger: %d%%\n", anger))
+	}
+	if contempt := round(face.Scores.Contempt * 100); contempt > 0 {
+		buffer.WriteString(fmt.Sprintf("Contempt: %d%%\n", contempt))
+	}
+	if disgust := round(face.Scores.Disgust * 100); disgust > 0 {
+		buffer.WriteString(fmt.Sprintf("Disgust: %d%%\n", disgust))
+	}
+	if fear := round(face.Scores.Fear * 100); fear > 0 {
+		buffer.WriteString(fmt.Sprintf("Fear: %d%%\n", fear))
+	}
+	if happiness := round(face.Scores.Happiness * 100); happiness > 0 {
+		buffer.WriteString(fmt.Sprintf("Happiness: %d%%\n", happiness))
+	}
+	if neutral := round(face.Scores.Neutral * 100); neutral > 0 {
+		buffer.WriteString(fmt.Sprintf("Neutral: %d%%\n", neutral))
+	}
+	if sadness := round(face.Scores.Sadness * 100); sadness > 0 {
+		buffer.WriteString(fmt.Sprintf("Sadness: %d%%\n", sadness))
+	}
+	if surprise := round(face.Scores.Surprise * 100); surprise > 0 {
+		buffer.WriteString(fmt.Sprintf("Surprise: %d%%\n", surprise))
+	}
+
+	return buffer.String()
 }
